658c2accbdc776612b8637b90ed5c87998f005f0
Jgopi07 <gopijangili123@gmail.com>
2025-03-11 22:55:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+            event.preventDefault(); 
+            document.getElementById("successMessage").style.display = "block"; 
+            this.reset(); 
+});
